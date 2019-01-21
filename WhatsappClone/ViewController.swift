74af46603d193730ed3a7a8432ccdb6061d2ee30
Eray <a@a-MacBook-Pro.local>
2019-01-21 14:35:54 +0300

    first commit

---
+++ WhatsappClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsappClone
+//
+//  Created by Eray on 21.01.2019.
+//  Copyright © 2019 Eray Alparslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
