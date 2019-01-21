48e6dde7fe87908c93e5c51a41ceef490ebabcf4
Eray <a@a-MacBook-Pro.local>
2019-01-21 16:56:36 +0300

    login page design

--- WhatsappClone/ViewController.swift
+++ WhatsappClone/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repasswordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+    }
 }
 
